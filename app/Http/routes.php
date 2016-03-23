de90b1f118c76bc67eb3e0ef2773b86034de60aa
Stdubic <stevandaleksic@gmail.com>
2016-03-23 13:10:36 +0100

    Adding auth fixing layout.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,8 +31,13 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
 
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+
     Route::resource('flyers', 'FlyersController');
     Route::get('{zip}/{street}', 'FlyersController@show');
     Route::post('{zip}/{street}/photos',['as' => 'store_photo_path' ,'uses' => 'FlyersController@addPhoto' ] );
 
 });
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,38 +1,64 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Flyer</title>
+    <title>Nesto Vredno</title>
   <link rel="stylesheet" href="/css/app.css">
   <link rel="stylesheet" href="/css/libs.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/dropzone.css">
 
 </head>
 
-<body>
+<body id="app-layout">
 
-       <nav class="navbar navbar-inverse navbar-fixed-top">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Project Flyer</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-              </ul>
-            </div><!--/.nav-collapse -->
-          </div>
-        </nav>
+       <nav class="navbar navbar-default navbar-static-top">
+               <div class="container">
+                   <div class="navbar-header">
+
+                       <!-- Collapsed Hamburger -->
+                       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                           <span class="sr-only">Toggle Navigation</span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                       </button>
+
+                       <!-- Branding Image -->
+                       <a class="navbar-brand" href="{{ url('/') }}">
+                           Nesto vredno
+                       </a>
+                   </div>
+
+                   <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                       <!-- Left Side Of Navbar -->
+                       <ul class="nav navbar-nav">
+                           <li><a href="{{ url('/home') }}">Home</a></li>
+                       </ul>
+
+                       <!-- Right Side Of Navbar -->
+                       <ul class="nav navbar-nav navbar-right">
+                           <!-- Authentication Links -->
+                           @if (Auth::guest())
+                               <li><a href="{{ url('/login') }}">Login</a></li>
+                               <li><a href="{{ url('/register') }}">Register</a></li>
+                           @else
+                               <li class="dropdown">
+                                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                       {{ Auth::user()->name }} <span class="caret"></span>
+                                   </a>
+
+                                   <ul class="dropdown-menu" role="menu">
+                                       <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                   </ul>
+                               </li>
+                           @endif
+                       </ul>
+                   </div>
+               </div>
+           </nav>
 
 
 
